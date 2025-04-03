d94adf3ac35705fb142bfe71276ec0af4e549715
mainnet-pat <mainnet_pat@protonmail.com>
2025-04-03 19:26:19 +0000

    Update relay peerid

--- packages/next-app/src/lib/RelayMultiaddrs.ts
+++ packages/next-app/src/lib/RelayMultiaddrs.ts
@@ -3,7 +3,7 @@ export const RelayMultiaddrs = [
   // "/ip4/192.168.0.15/tcp/33333/ws/p2p/12D3KooWNVCCbMtGc1SSPVdjyJDqC728RtWezvzyvEiFEn8zSbXH",
   // "/ip4/192.168.64.1/tcp/33333/ws/p2p/12D3KooWNVCCbMtGc1SSPVdjyJDqC728RtWezvzyvEiFEn8zSbXH",
   // "/ip4/10.6.0.2/tcp/33333/ws/p2p/12D3KooWNVCCbMtGc1SSPVdjyJDqC728RtWezvzyvEiFEn8zSbXH",
-  "/dns4/axe-relay.pat.mn/tcp/443/wss/p2p/12D3KooWRxd4SnpR3hkbxdfrge3ub9LPfcbvC42d9vF82SftL4qi",
+  "/dns4/axe-relay.pat.mn/tcp/443/wss/p2p/12D3KooWNNtP4zGRwDLG9j6n7MCcuzbThJvBTSuywJC1gmWcNfmj",
 ]
 
 export const PeerIdToRelayMultiaddrs = (peerId: string) => {
